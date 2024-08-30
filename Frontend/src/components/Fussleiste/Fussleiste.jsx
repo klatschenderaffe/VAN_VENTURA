5288d5832cfb2c7364dcce138db42a8932c4602d
Pia Heiß <pia.heiss@edu.techstarter.de>
2024-08-30 09:38:46 +0200

    Kleinere Schönheiten verändert

--- Frontend/src/components/Fussleiste/Fussleiste.jsx
+++ Frontend/src/components/Fussleiste/Fussleiste.jsx
@@ -1,18 +1,17 @@
 import React from 'react';
 import './Fussleiste.css';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-import Impressum from '../Impressum/Impressum';
+
 
 
 const Fussleiste = () => {
   return (
 
-      <footer className='fussleiste'>
-        <ul>
+    <footer className='fussleiste'>
+      <ul>
         <li>Impressum</li>
-        </ul>
-      </footer>
-      
+      </ul>
+    </footer>
+
   );
 };
 
